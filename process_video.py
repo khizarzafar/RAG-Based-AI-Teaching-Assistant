33aca4918a62526902575ada6b0df999fb557824
Your Name <khizarzafar19@gmail.com>
2025-12-20 15:02:58 +0500

    chore: remove large files and push clean code

--- process_video.py
+++ process_video.py
@@ -1,1 +1,11 @@
-import whisper
+# import whisper
+import sys
+sys.stdout.reconfigure(encoding='utf-8') #encoding error fix
+
+import os
+
+files = os.listdir("Course_Videos")
+for file in files:
+    tutorial_number = file.split('-')[0].strip()
+    file_name = file.split('-')[1].strip().split('｜')[0].strip()
+    print(tutorial_number,file_name)
